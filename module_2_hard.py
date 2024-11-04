cf71841e2b15b5a27babef0798a1990b3fb2eaf7
Aslan <fichil53@gmail.com>
2024-11-04 06:02:36 +0300

    2

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,9 +1,9 @@
 n = int(input("Введите число от 3 до 20: "))
-i = ""
+f = ""
 
 for i in range(1, n):
     for j in range(i + 1, n + 1):
         if n % (i + j) == 0:
-            i += f"{i}{j}"
+            f += f"{i}{j}"
 
-print("Нужный пароль:", i)
+print("Нужный пароль:", f)
